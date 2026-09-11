b9cc06bb7fc2304d3ff7a6989782e278dbde8bab
wz-Tan <wenzhe1423@gmail.com>
2026-09-11 13:28:47 +0800

    feat: concurrent mutex storage

---
+++ internal/booking/concurrent_store.go
@@ -0,0 +1,43 @@
+package booking
+
+import "sync"
+
+type ConcurrentStore struct {
+	bookings     map[string]Booking
+	sync.RWMutex // for mutex
+}
+
+func NewConcurrentStore() *ConcurrentStore {
+	return &ConcurrentStore{
+		bookings: map[string]Booking{},
+	}
+}
+
+func (s *ConcurrentStore) Book(b Booking) error {
+	s.Lock()
+	defer s.Unlock()
+
+	// Seat Already Booked
+	if _, exists := s.bookings[b.SeatID]; exists {
+		return ErrSeatAlreadyBooked
+	}
+
+	// Seat Not Booked
+	s.bookings[b.SeatID] = b
+	return nil
+}
+
+func (s *ConcurrentStore) ListBookings(movieID string) []Booking {
+	s.RLock()
+	s.RUnlock()
+
+	var movieBookings []Booking
+
+	for _, booking := range s.bookings {
+		if booking.MovieID == movieID {
+			movieBookings = append(movieBookings, booking)
+		}
+	}
+
+	return movieBookings
+}
--- internal/booking/service_test.go
+++ internal/booking/service_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestConcurrentBooking_ExactlyOneWins(t *testing.T) {
-	store := NewMemoryStore()
+	store := NewConcurrentStore()
 	svc := NewService(store)
 
 	const numGoroutines = 100_000 // 100k users trying to book a seat at the same time
